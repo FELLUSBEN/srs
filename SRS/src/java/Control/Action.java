a0e4ff4724e0b9a6d7dfa8184c87165ec7fa0c89
razi chai <razi.chai@gmail.com>
2023-10-01 18:33:34 +0300

    added action

--- SRS/src/java/Control/Action.java
+++ SRS/src/java/Control/Action.java
@@ -4,8 +4,10 @@
  */
 package Control;
 
+import Model.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -57,7 +59,60 @@ public class Action extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        if(request.getParameter("act").equals("A")){
+//            Restaurant r = new Restaurant(((User)request.getSession().getAttribute("usr")).getUsr(),request.getParameter("name"),request.getParameter("link"),request.getParameter("desc"),Boolean.parseBoolean(request.getParameter("pub")));
+//            if(Model.Maneger.isExists(r)){
+//                request.setAttribute("msg", "exist");
+//                request.getRequestDispatcher("Massege").forward(request, response);
+//            }else{
+//                Model.Maneger.Add(r);
+//                ((User)request.getSession().getAttribute("usr")).setAdded(s.getName());
+//                request.getRequestDispatcher("Main").forward(request, response);
+//            } *******add annaons - meneger function needed*********
+        }
+        
+        else if(request.getParameter("act").equals("S")){
+            String[] sparams={request.getParameter("name"), request.getParameter("desc")};
+            request.getSession().setAttribute("search", sparams[0]+","+sparams[1]);
+            ArrayList<Restaurant> rs= Model.Maneger.Serch(((User)request.getSession().getAttribute("usr")).getUsr(), sparams[0], sparams[1]);
+            request.setAttribute("content", (rs == null)? "X":rs); 
+            request.getRequestDispatcher("Display.jsp").forward(request, response);
+        }else{
+            String[] params=request.getParameter("act").split(",");
+            String[] sparams=((String)request.getSession().getAttribute("search")).split(",");
+            if(sparams.length == 0){
+                String[] s = {"" , ""};
+                sparams = s;
+            }
+            if(params[0].equals("U")){
+                if(request.getParameter("name") == null)
+                    request.getRequestDispatcher("Update.jsp").forward(request, response);
+                else{
+                    Restaurant r1 = new Restaurant(params[1],params[2],params[3],params[4],params[5],Integer.parseInt(params[6]),Integer.parseInt(params[7]),Integer.parseInt(params[8]),Integer.parseInt(params[9]),params[5]);
+                    Restaurant r2 = new Restaurant(params[1],params[2],request.getParameter("name"),request.getParameter("address"),request.getParameter("employees"),Integer.parseInt(request.getParameter("seats")),Integer.parseInt(request.getParameter("Fseats")),Integer.parseInt(request.getParameter("pr")),Integer.parseInt(request.getParameter("Fpr")),request.getParameter("Ftype"));
+                    
+                    if(Model.Maneger.isExists(r2)){
+                        Model.Maneger.Add(r1);
+                        request.setAttribute("msg", "exist");
+                        request.getRequestDispatcher("Massege").forward(request, response);
+                    }else{
+                        Model.Maneger.Delete(r1);
+                        Model.Maneger.Add(r2);
+//                        ArrayList<Restaurant> rs= Model.Maneger.Serch(params[1] ,sparams[0], sparams[1]);
+//                        request.setAttribute("content", (rs == null)? "X":rs); 
+//                        request.getRequestDispatcher("Display.jsp").forward(request, response);        ******might be helpfull*******
+                    }
+                }
+            }
+//            else{
+//                Model.Maneger.Delete(new Restaurant(params[1],params[2],"","","",0,0,0,0,""));
+//                ArrayList<Restaurant> rs= Model.Maneger.Serch(params[1] ,sparams[0], sparams[1]);
+//                request.setAttribute("content", (rs == null)? "X":rs); 
+//                request.getRequestDispatcher("Display.jsp").forward(request, response);
+//            } ******optional: add delete restaurant
+        }
+        
+        response.sendRedirect("Main");
     }
 
     /**
@@ -71,7 +126,13 @@ public class Action extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        if(request.getParameter("act").equals("A")){
+            request.getRequestDispatcher("Add.jsp").forward(request, response);
+        }else if(request.getParameter("act").equals("E")){
+            request.getRequestDispatcher("Exit.jsp").forward(request, response);
+        }else if(request.getParameter("act").equals("S")){
+            request.getRequestDispatcher("Display.jsp").forward(request, response);
+        }
     }
 
     /**
