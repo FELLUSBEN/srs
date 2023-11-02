80a96832fcf66230f92ceab32392e1af8b776a7f
Ben Fellus <50238518+FELLUSBEN@users.noreply.github.com>
2023-11-02 15:19:23 +0200

    update

--- SRS/src/java/Model/Announcement.java
+++ SRS/src/java/Model/Announcement.java
@@ -12,7 +12,7 @@ public class Announcement {
     private String usr;
     private String titel;
     private String desc;
-    private Date date;
+    private long date;
     private String dest;
     private static int dayOfYear;
     
@@ -20,7 +20,7 @@ public class Announcement {
         this.usr = usr;
         this.titel = titel;
         this.desc = desc;
-        this.date = new Date(date);
+        this.date = date;
         this.dest=dest;
     }
 
@@ -28,11 +28,11 @@ public class Announcement {
         this.usr = usr;
         this.titel = titel;
         this.desc = desc;
-        this.date = new Date();
+        this.date = new Date().getTime();
         this.dest = dest;
     }
 
-    public Date getDate() {
+    public long getDate() {
         return date;
     }
 
