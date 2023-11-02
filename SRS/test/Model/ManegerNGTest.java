f0e88b3ce8dc56b470532b07e07bedd44d2b94db
razi chai <razi.chai@gmail.com>
2023-11-03 00:17:05 +0200

    unitesting almost done

--- SRS/test/Model/ManegerNGTest.java
+++ SRS/test/Model/ManegerNGTest.java
@@ -18,7 +18,9 @@ import org.testng.annotations.Test;
  */
 public class ManegerNGTest {
     
-    Maneger instance = Maneger.getInstance();
+    private Maneger instance = Maneger.getInstance();
+    private User existUser = new User("Usr","Test");
+    private User fakeUser = new User("stam","fantom");
     
     public ManegerNGTest() {
     }
@@ -76,137 +78,137 @@ public class ManegerNGTest {
         assertEquals(result,testorant,"couldn't find restaurant");
     }
     // ==== check inside the database for changes ====
-    @Test
-    public void testAdd_Castomer() {
-        System.out.println("Add");
-        Castomer x = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Add(x);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
-    @Test
-    public void testAdd_Restaurant() {
-        System.out.println("Add");
-        Restaurant r = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Add(r);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testAdd_Announcement() {
-        System.out.println("Add");
-        Announcement a = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Add(a);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testUpdate_Castomer_Castomer() {
-        System.out.println("Update");
-        Castomer c1 = null;
-        Castomer c2 = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Update(c1, c2);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testUpdate_Restaurant_Restaurant() {
-        System.out.println("Update");
-        Restaurant r1 = null;
-        Restaurant r2 = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Update(r1, r2);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    // ===========================================
-    
     
     @Test
     public void testIsExists_Castomer() {
         System.out.println("isExists");
-        Castomer c = null;
-        Maneger instance = Maneger.getInstance();
-        boolean = false;
+        
+        Castomer c = new Castomer("Raz","123","razi.chai@gmail.com");
         boolean result = instance.isExists(c);
-        assertEquals( result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertTrue(result, "couldn't find Castomer");
+        
+        c = new Castomer("ben","clone","benTzhokim@gmail.com");
+        result = instance.isExists(c);
+        assertFalse(result, "found non existent castomer");
     }
-
+    
     @Test
     public void testIsExists_Restaurant() {
         System.out.println("isExists");
-        Restaurant r = null;
-        Maneger instance = Maneger.getInstance();
-        boolean = false;
-        boolean result = instance.isExists(r);
-        assertEquals( result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Restaurant testorant = new Restaurant("Raz","333","BurgeRaz","","",0,0,0,0,"");
+        boolean result = instance.isExists(testorant);
+        assertTrue(result, "couldn't find Castomer");
+        
+        testorant = new Restaurant("ben","clone","","","",0,0,0,0,"");
+        result = instance.isExists(testorant);
+        assertFalse(result, "found non existent castomer");
     }
-
+    
     @Test
     public void testSearch() {
-        System.out.println("Search");
-        String name = "";
-        String type = "";
-        Maneger instance = Maneger.getInstance();
-        ArrayList<Restaurant> = null;
-        ArrayList<Restaurant> result = instance.Search(name, type);
-        assertEquals( result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       System.out.println("Search");
+       
+       ArrayList<Restaurant> expected = new ArrayList<Restaurant>();
+       expected.add(new Restaurant("Raz","333","BurgeRaz","","",0,0,0,0,""));
+       assertEquals(expected, instance.Search("Raz", ""),"couldn't find restaurant properly");
+        
+       assertNull(instance.Search("ben", "clone"),"couldn't find restaurant properly");
     }
-
-    @Test
-    public void testDelete_Castomer() {
-        System.out.println("Delete");
-        Castomer c = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Delete(c);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testDelete_Restaurant() {
-        System.out.println("Delete");
-        Restaurant r = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Delete(r);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    
-    @Test
-    public void testDelete_Announcement() {
-        System.out.println("Delete");
-        Restaurant r = null;
-        Maneger instance = Maneger.getInstance();
-        instance.Delete(r);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
-    @Test
-    public void testGetAnnouncements() {
-        System.out.println("getAnnouncements");
-        Maneger instance = Maneger.getInstance();
-        ArrayList<Announcement> = null;
-        ArrayList<Announcement> result = instance.getAnnouncements(new User("stam","check"));
-        assertEquals( result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+//    
+//    @Test
+////    public void testAdd_Castomer() {
+////        System.out.println("Add");
+////        Castomer x = null;
+////        Maneger instance = Maneger.getInstance();
+////        instance.Add(x);
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+////    }
+////    
+////    @Test
+////    public void testAdd_Restaurant() {
+////        System.out.println("Add");
+////        Restaurant r = null;
+////        Maneger instance = Maneger.getInstance();
+////        instance.Add(r);
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+////    }
+////
+////    @Test
+////    public void testAdd_Announcement() {
+////        System.out.println("Add");
+////        Announcement a = null;
+////        Maneger instance = Maneger.getInstance();
+////        instance.Add(a);
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+////    }
+////
+////    @Test
+////    public void testUpdate_Castomer_Castomer() {
+////        System.out.println("Update");
+////        Castomer c1 = null;
+////        Castomer c2 = null;
+////        Maneger instance = Maneger.getInstance();
+////        instance.Update(c1, c2);
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+////    }
+////
+////    @Test
+////    public void testUpdate_Restaurant_Restaurant() {
+////        System.out.println("Update");
+////        Restaurant r1 = null;
+////        Restaurant r2 = null;
+////        Maneger instance = Maneger.getInstance();
+////        instance.Update(r1, r2);
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+////    }
+////    // ===========================================
+//
+//    @Test
+//    public void testDelete_Castomer() {
+//        System.out.println("Delete");
+//        Castomer c = null;
+//        Maneger instance = Maneger.getInstance();
+//        instance.Delete(c);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testDelete_Restaurant() {
+//        System.out.println("Delete");
+//        Restaurant r = null;
+//        Maneger instance = Maneger.getInstance();
+//        instance.Delete(r);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    
+//    @Test
+//    public void testDelete_Announcement() {
+//        System.out.println("Delete");
+//        Restaurant r = null;
+//        Maneger instance = Maneger.getInstance();
+//        instance.Delete(r);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+//    @Test
+//    public void testGetAnnouncements() {
+//        System.out.println("getAnnouncements");
+//        Maneger instance = Maneger.getInstance();
+//        ArrayList<Announcement> = null;
+//        ArrayList<Announcement> result = instance.getAnnouncements(new User("stam","check"));
+//        assertEquals( result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
 }
